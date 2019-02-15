f09fd7e2ebf488612876826ae97fd94952c03149
Rafael Schouten <rafaelschouten@gmail.com>
2019-02-15 11:36:36 +1100

    initialise

---
+++ README.md
@@ -0,0 +1,20 @@
+# Cellular
+
+[![Build Status](https://travis-ci.org/rafaqz/Cellular.jl.svg?branch=master)](https://travis-ci.org/rafaqz/Cellular.jl) 
+[![Build status](https://ci.appveyor.com/api/projects/status/hgapxluxfsypvptc?svg=true)](https://ci.appveyor.com/project/rafaqz/cellular-jl)
+[![Coverage Status](https://coveralls.io/repos/rafaqz/Cellular.jl/badge.svg?branch=master&service=github)](https://coveralls.io/github/rafaqz/Cellular.jl?branch=master) 
+[![codecov.io](http://codecov.io/github/rafaqz/Cellular.jl/coverage.svg?branch=master)](http://codecov.io/github/rafaqz/Cellular.jl?branch=master)
+
+Cellular is a generalised, modular framework for cellular automata and other
+cellular models.
+
+This package is extended by [Dispersal.jl](https://github.com/rafaqz/Dispersal.jl)
+for modelling organism dispersal.
+
+```julia
+Pkg.clone("https://github.com/rafaqz/Cellular.jl"
+```
+
+## Documentation
+
+- [**LATEST**](https://rafaqz.github.io/Cellular.jl/latest/) &mdash; *in-development version of the documentation.*
---
+++ src/Apply.jl
@@ -0,0 +1,44 @@
+module Apply
+
+export apply, offset_apply
+using Base: tail
+
+apply!(f::F, a::Tuple{A,Vararg}, args...) where {F,A} = 
+    (f(a[1], args...), apply(f, tail(a), args...)...)
+apply!(f::F, a::Tuple{A,Vararg}, b::Tuple{B,Vararg}, args...) where {F,A,B} = 
+    (f(a[1], b[1], args...), apply(f, tail(a), tail(b), args...)...)
+apply!(f::F, a::Tuple{A,Vararg}, b::Tuple{B,Vararg}, c::Tuple{C,Vararg}, args...) where {F,A,B,C} = 
+    (f(a[1], b[1], c[1], args...), apply(f, tail(a), tail(b), tail(c), args...)...)
+apply!(f::F, a::Tuple{A,Vararg}, b::Tuple{B,Vararg}, c::Tuple{C,Vararg}, d::Tuple{D,Vararg}, args...) where {F,A,B,C,D} = 
+    (f(a[1], b[1], c[1], d[1], args...), apply(f, tail(a), tail(b), tail(c), tail(d), args...)...)
+apply!(f, a, b, ::Tuple{}, args...) = ()
+apply!(f, a, b, c, ::Tuple{}, args...) = ()
+apply(f, ::Tuple{}, ::Tuple{}, ::Tuple{}, args...) = nothing
+apply(f, a, ::Tuple{}, ::Tuple{}, args...) = nothing
+apply(f, ::Tuple{}, b, ::Tuple{}, args...) = nothing
+apply(f, a, b, ::Tuple{}, args...) = nothing
+apply(f, ::Tuple{}, ::Tuple{}, args...) = nothing
+apply(f, a, ::Tuple{}, args...) = nothing
+apply(f, ::Tuple{}, args...) = nothing
+
+
+offset_apply!(f, o::Tuple{O,Vararg}, a::AbstractArray, offset::Int, args...) where O = begin
+    offset = f(o[1], a, offset, args...)
+    offset_apply!(f, tail(o), a, offset::Int, args...)
+    nothing
+end
+offset_apply!(f, a::AbstractArray, o::Tuple{O,Vararg}, offset::Int, args...) where O = begin
+    offset = f(a, o[1], offset, args...)
+    offset_apply!(f, a, tail(o), offset::Int, args...)
+    nothing
+end
+offset_apply!(f, o::Tuple{O,Vararg}, offset::Int, args...) where O = begin
+    offset = f(o[1], offset, args...)
+    offset_apply!(f, tail(o), offset::Int, args...)
+    nothing
+end
+offset_apply!(f, o::Tuple{}, offset::Int, args...) = nothing
+offset_apply!(f, a::AbstractArray, o::Tuple{}, offset::Int, args...) = nothing
+offset_apply!(f, o::Tuple{}, a::AbstractArray, offset::Int, args...) = nothing
+
+end # module
